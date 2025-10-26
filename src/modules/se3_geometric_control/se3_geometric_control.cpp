78e74089f479878e8c63cb31c0fc16c966c62ab0
he <shuishuidehentiexin@gmail.com>
2025-10-26 19:15:50 +0800

    start se3 controller project

---
+++ src/modules/se3_geometric_control/se3_geometric_control.cpp
@@ -0,0 +1,7 @@
+#include <pinocchio/parsers/urdf.hpp>
+#include <pinocchio/algorithm/model.hpp>
+#include <pinocchio/algorithm/kinematics.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/rnea.hpp>
+#include "se3_geometric_control.hpp"
